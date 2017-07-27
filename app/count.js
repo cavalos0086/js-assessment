20eda426416f7cc84bdd79a90e5ad6910fbc38a1
Cristian E. Avalos <cavalos0086@gmail.com>
2017-07-27 15:11:12 -0400

    finishes count

--- app/count.js
+++ app/count.js
@@ -1,7 +1,22 @@
 exports = typeof window === 'undefined' ? global : window;
 
 exports.countAnswers = {
-  count: function (start, end) {
+	count: function (start, end) {
+		var i = start;
+		var timeoutID;
+		function print() {
+			console.log(i);
+			i++;
+			if (i <= end) {
+				timeoutID = setTimeout(print, 100);
+			}
+		}
+		print();
 
-  }
+		return {
+			cancel: function() {
+				clearTimeout(timeoutID);
+			}
+		};
+	}
 };
