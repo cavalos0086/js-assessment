a6e58ececfae18708ef472e2ffa10af2099cf7f1
Cristian E. Avalos <cavalos0086@gmail.com>
2017-07-27 00:36:49 -0400

    finish async

--- app/async.js
+++ app/async.js
@@ -1,11 +1,27 @@
 exports = typeof window === 'undefined' ? global : window;
 
 exports.asyncAnswers = {
-  async: function(value) {
+	async: function (value) {
+		return Promise.resolve(value);
+	},
 
-  },
+	manipulateRemoteData: function (url) {
+		return new Promise(function(resolve, reject) {
+			var call = new XMLHttpRequest();
+			call.open('GET', url);
+			call.onload = function() {
+				var tempData = JSON.parse(call.responseText).people;
+				var names = [];
 
-  manipulateRemoteData: function(url) {
-
-  }
+				for (var i = 0; i < tempData.length; i++) {
+					names.push(tempData[i].name);
+				}
+				resolve(names.sort());
+			};
+			call.onerror = function() {
+				reject(JSON.parse(call.statusText));
+			};
+			call.send();
+		});
+	}
 };
