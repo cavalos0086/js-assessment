17d56420dda8537425c979446cfaacb064c43808
Cristian E. Avalos <cavalos0086@gmail.com>
2017-07-29 00:18:20 -0400

    missing one function

--- app/functions.js
+++ app/functions.js
@@ -1,39 +1,65 @@
 exports = typeof window === 'undefined' ? global : window;
 
 exports.functionsAnswers = {
-  argsAsArray: function(fn, arr) {
+	argsAsArray: function (fn, arr) {
+		return fn.apply(null, arr);
+	},
 
-  },
+	speak: function (fn, obj) {
+		return fn.call(obj);
+	},
 
-  speak: function(fn, obj) {
+	functionFunction: function (str) {
+		return function(str2) {
+			return str + ', ' + str2;
+		};
+	},
 
-  },
+	makeClosures: function (arr, fn) {
+		var funcArray = [];
 
-  functionFunction: function(str) {
+		funcArray = arr.map(function(item) {
+			return function() {
+				return fn(item);
+			};
+		});
 
-  },
+		return funcArray;
+	},
 
-  makeClosures: function(arr, fn) {
+	partial: function (fn, str1, str2) {
+		return function(str3) {
+			return fn.call(null, str1, str2, str3);
+		};
+	},
 
-  },
+	useArguments: function () {
+		var args = Array.prototype.slice.call(arguments);
+		var sum = 0;
+		for (var i = 0; i < args.length; i++) {
+			sum += args[i];
+		}
 
-  partial: function(fn, str1, str2) {
+		return sum;
+	},
 
-  },
+	callIt: function (fn) {
+		var args = Array.prototype.slice.call(arguments);
+		// we need to slice(1) because callIt gets call with a fn, and then the arguments of fn.
+		return fn.apply(null, args.slice(1));
+	},
 
-  useArguments: function() {
+	partialUsingArguments: function (fn) {
+		var args = [].slice.call(arguments, 1, arguments.length);
 
-  },
+		return function() {
+			var moreArgs = args.concat([].slice.call(arguments));
+			return fn.apply(null, moreArgs);
+		}
 
-  callIt: function(fn) {
+	},
 
-  },
+	curryIt: function (fn) {
 
-  partialUsingArguments: function(fn) {
-
-  },
-
-  curryIt: function(fn) {
-
-  }
+	}
 };
